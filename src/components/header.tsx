1db001ba7841f51ab9778a2968272d4acc1db6e5
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-13 09:34:35 +0000

    اديلو مارجين لفت 20 بيكسل

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-6 flex items-center space-x-2 group transition-transform duration-300 hover:scale-105">
+        <Link href="/" className="mr-6 flex items-center space-x-2 group transition-transform duration-300 hover:scale-105 ml-5">
           <ShoppingCart className="h-8 w-8 text-primary" />
           <span className="font-extrabold font-headline inline-block text-3xl">
             Do<span className="text-primary">m</span>a
