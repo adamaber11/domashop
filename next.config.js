ede23bab836a6aeaf75717064b6a88e469aebf18
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-14 08:17:26 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -39,6 +39,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.apple.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
