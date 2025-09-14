3c5270e217a50fb3dfedddd93271fde7bf903cdf
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-14 08:11:19 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
