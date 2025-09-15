0fa638e5d1b4028c1f08bbe2f06c3e69a71f40cd
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-09-15 05:16:01 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,',
+        protocol: 'https',
         hostname: 'www.google.com',
         port: '',
         pathname: '/**',
